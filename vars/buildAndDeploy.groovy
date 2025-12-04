6444ed3ebc181b243bd8e61f8a8efc5b56751d27
Gherafa <gheorf51@gmail.com>
2025-12-04 14:11:26 +0700

    [automation] fix variable value

--- vars/buildAndDeploy.groovy
+++ vars/buildAndDeploy.groovy
@@ -1,6 +1,6 @@
 def call(Map config = [:]) {
     // load pipeline from resources folder
-    def pipelineScript = libraryResource('buildAndDeployPipeline.groovy')
+    def pipelineScript = libraryResource('buildAndDeployScript.groovy')
 
     // evaluate it as a pipeline, injecting your config into it
     evaluate(pipelineScript)
