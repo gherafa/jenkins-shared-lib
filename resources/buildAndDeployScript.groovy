5db27c186083179b8defb3012704373d6357ac5f
Gherafa <gheorf51@gmail.com>
2025-12-04 14:30:45 +0700

    [automation] adjust script

--- resources/buildAndDeployScript.groovy
+++ resources/buildAndDeployScript.groovy
@@ -15,18 +15,6 @@ pipeline {
             }
         }
 
-        stage('Install Dependencies') {
-            steps {
-                sh "npm install"
-            }
-        }
-
-        stage('Run Tests') {
-            steps {
-                sh "npm test"
-            }
-        }
-
         stage('Build Docker Image') {
             steps {
                 sh "docker build -t ${IMAGE}:${TAG} ."
